0a099be9720d3e67947c3998cbb25dc5606a7c23
hoalongnatsu <hoalongnatsu@gmail.com>
2021-12-27 12:02:08 +0700

    update

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	if name == "" {
-		name = "World Update"
+		name = "World VM"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", name)
 }
